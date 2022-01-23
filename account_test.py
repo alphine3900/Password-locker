d90bcda42b6e6696128571d4ca5a2389a9b2626a
alphine3900 <alphine.kirimi@student.moringaschool.com>
2022-01-23 19:25:48 +0300

    added functionality to account test file

--- account_test.py
+++ account_test.py
@@ -55,3 +55,23 @@ def test_find_account_by_username(self):
         test_account.save_account()
         found_account = Accounts.find_by_user_name('ijkl')
         self.assertEqual(found_account.user_name, test_account.user_name)
+        def test_account_exists(self):
+        '''
+        unlike the previous test this test returns a true/false soort of answer depending on whether the account exists or not
+        '''
+        self.new_account.save_account()
+        test_account = Accounts('abcd', 'efgh', 'ijkl', 'mnop')
+        test_account.save_account()
+        account_exists = Accounts.account_exists('ijkl')
+        self.assertTrue(account_exists)
+
+def test_display_accounts(self):
+        '''
+        a test to check the display accounts function
+        '''
+        self.assertEqual(Accounts.display_accounts(),
+                         Accounts.user_accounts)
+
+
+if __name__ == '__main__':
+    unittest.main()
