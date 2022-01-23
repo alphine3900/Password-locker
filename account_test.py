772349a58a93d2f1aa017a12efbf1360c4fdee27
alphine3900 <alphine.kirimi@student.moringaschool.com>
2022-01-23 19:25:00 +0300

    added functionality to account test file

--- account_test.py
+++ account_test.py
@@ -28,3 +28,30 @@ def test_save_account(self):
         self.assertEqual(len(Accounts.user_accounts), 1)
 
 def test_save_multiple_accounts(self):
+           '''
+        a test that checks whether both values appended to the array are actually present\ and returns the acount itself
+        '''
+        self.new_account.save_account()
+        test_account = Accounts('abcd', 'efgh', 'ijkl', 'mnop')
+        test_account.save_account()
+        self.assertEqual(len(Accounts.user_accounts), 2)
+
+def test_del_account(self):
+        '''
+        test that check the delete function
+        '''
+        self.new_account.save_account()
+        test_account = Accounts('abcd', 'efgh', 'ijkl', 'mnop')
+        test_account.save_account()
+        self.new_account.delete_account()
+        self.assertEqual(len(Accounts.user_accounts), 1)
+
+def test_find_account_by_username(self):
+        '''
+        test to check whether the function used to find accounts really works
+        '''
+        self.new_account.save_account()
+        test_account = Accounts('abcd', 'efgh', 'ijkl', 'mnop')
+        test_account.save_account()
+        found_account = Accounts.find_by_user_name('ijkl')
+        self.assertEqual(found_account.user_name, test_account.user_name)
