dfd5b372f7c5cda4e368bccf387a8675d63969ce
alphine3900 <alphine.kirimi@student.moringaschool.com>
2022-01-23 20:42:43 +0300

    added some functions in sign up

--- account_test.py
+++ account_test.py
@@ -9,16 +9,16 @@ class TestAccount(unittest.TestCase):
         '''
         this test runs before every test occurs
         '''
-        self.new_account = Accounts('hamida', 'mstafa', 'mids', '1996')
+        self.new_account = Accounts('alphine', 'mutwiri', 'alvo', '2000')
 
     def test_init(self):
         '''
         a test to assert whether the values entered would appear when the roperty is called
         '''
-        self.assertEqual(self.new_account.first_name, 'hamida')
-        self.assertEqual(self.new_account.last_name, 'mstafa')
-        self.assertEqual(self.new_account.user_name, 'mids')
-        self.assertEqual(self.new_account.password, '1996')
+        self.assertEqual(self.new_account.first_name, 'alphine')
+        self.assertEqual(self.new_account.last_name, 'mutwiri')
+        self.assertEqual(self.new_account.user_name, 'alvo')
+        self.assertEqual(self.new_account.password, '2000')
 +9
 def test_save_account(self):
         '''
